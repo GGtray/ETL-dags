9679071105130d474f94e1ffc1e78c0bc872afca
Lizi Zhang <liritian1994@gmail.com>
2020-06-18 01:47:38 -0500

    nothing

--- udac_example_dag.py
+++ udac_example_dag.py
@@ -26,36 +26,8 @@ start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
 # end-date:     'log_data/2018/11/2018-11-30-events.json'
 
 
-stage_events_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_events',
-    dag=dag,
-    table="public.staging_events",
-    redshift_conn_id="redshift",
-    aws_credentials_id="aws_credentials_root",
-    s3_bucket="s3_bucket",
-    s3_key="log_data",
-    # s3_key='log_data/2018/11/{}-{}-{}-events.json',
-    region="us-west-2",
-    create_table=staging_events_table_create,
-    format="json 's3://udacity-dend/log_json_path.json'",
-    provide_context=True
-)
-
-stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_songs',
-    dag=dag,
-    table="public.staging_songs",
-    redshift_conn_id="redshift",
-    aws_credentials_id="aws_credentials_root",
-    s3_bucket="s3_bucket",
-    s3_key="song_data",
-    region="us-west-2",
-    create_table=staging_songs_table_create,
-    format="json 'auto'",
-    provide_context=True
-)
 
 end_operator = DummyOperator(task_id='Stop_execution', dag=dag)
 
-start_operator >> stage_events_to_redshift
-start_operator >> stage_songs_to_redshift >> end_operator
+
+start_operator >>  end_operator
